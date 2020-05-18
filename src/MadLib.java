af093d3388974641c0b8fe021327c2e2bd5cca4a
Tyler Mallett <tmallett230@students.ccsnh.edu>
2020-05-18 11:05:56 -0400

    Zylab 1.10 MadLib Complete

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class MadLib {
-    //Starting zyLab MadLib
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String firstName;
+        String genericLocation;
+        int wholeNumber;
+        String pluralNoun;
+
+        firstName = scnr.next(); //stores a string up to the first space
+        genericLocation = scnr.next();
+        wholeNumber = scnr.nextInt();
+        pluralNoun = scnr.next();
+
+        System.out.println(firstName + " went to " + genericLocation + " to buy " + wholeNumber + " different types of " + pluralNoun + ".");
+    }
 }
+
